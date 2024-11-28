d9d9ab2ae86d61efb8acec3eac40bdb8f70ba17b
Petrik Diak <diak@petrik.hu>
2024-11-28 16:15:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bevasarlo_alkalmazas/ShoppingListAdapter.java
@@ -0,0 +1,12 @@
+package com.example.bevasarlo_alkalmazas;
+
+import android.content.Context;
+import android.hardware.display.DeviceProductInfo;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class ShoppingListAdapter extends BaseAdapter {
+        private final Context context;
+        private final List<Product> productList;
+}
